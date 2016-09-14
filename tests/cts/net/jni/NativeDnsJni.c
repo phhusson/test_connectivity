07f9388e4fe84f13fd3f345e4db9dc9b49d982ed
Stephen Li <stephenli@google.com>
2016-09-14 22:25:06 +0000

    Revert "Manually merge commit '9be9d5865ba2584a251642359710c074061dee5e' into nyc-dev"
    This reverts commit 253fdc667a18ec0cfdc511c7d21e3b308ecb9cfb.
    
    Change-Id: If8862e289eb6693650713ff819c8b05f65cdeda5

--- tests/cts/net/jni/NativeDnsJni.c
+++ tests/cts/net/jni/NativeDnsJni.c
@@ -126,7 +126,7 @@ JNIEXPORT jboolean Java_android_net_cts_DnsTest_testNativeDns(JNIEnv* env, jclas
         return JNI_FALSE;
     }
 
-    memset(buf, 0, sizeof(buf));
+    memset(buf, sizeof(buf), 0);
     res = getnameinfo((const struct sockaddr*)&sa6, sizeof(sa6), buf, sizeof(buf), NULL, 0, flags);
     if (res != 0) {
         ALOGD("getnameinfo(%s (GoogleDNS) ) gave error %d (%s)", GoogleDNSIpV6Address2,
