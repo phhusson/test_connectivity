f54b3dd76a63c4032372dbe0af123e7c9b251cae
Sudheer Shanka <sudheersai@google.com>
2018-01-11 12:44:58 -0800

    Update PROCESS_STATE_FOREGROUND_SERVICE constant value.
    Bug: 71758358
    Test: cts-tradefed run singleCommand cts-dev -m CtsHostsideNetworkTests -t \
          com.android.cts.net.HostsideRestrictBackgroundNetworkTests
    
    Change-Id: Ia4509e94e33db5b61bf1aea704d25a199d381494

--- tests/cts/hostside/app/src/com/android/cts/net/hostside/AbstractRestrictBackgroundNetworkTestCase.java
+++ tests/cts/hostside/app/src/com/android/cts/net/hostside/AbstractRestrictBackgroundNetworkTestCase.java
@@ -87,7 +87,7 @@ abstract class AbstractRestrictBackgroundNetworkTestCase extends Instrumentation
     private static final String NETWORK_STATUS_SEPARATOR = "\\|";
     private static final int SECOND_IN_MS = 1000;
     static final int NETWORK_TIMEOUT_MS = 15 * SECOND_IN_MS;
-    private static final int PROCESS_STATE_FOREGROUND_SERVICE = 4;
+    private static final int PROCESS_STATE_FOREGROUND_SERVICE = 3;
     private static final int PROCESS_STATE_TOP = 2;
 
     private static final String KEY_NETWORK_STATE_OBSERVER = TEST_PKG + ".observer";
